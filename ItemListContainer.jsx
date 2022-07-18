72bc6686c239b0736331d24c736bb48d7ba6b958
Mayra098 <83836523+Mayra098@users.noreply.github.com>
2022-07-17 23:58:47 -0300

    Delete ItemListContainer.jsx

--- ItemListContainer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ItemListContainer = ({greeting}) => {
-    return (
-      <div className='bien'>{greeting}</div>
-    )
-  }
-  
-  export default ItemListContainer 
